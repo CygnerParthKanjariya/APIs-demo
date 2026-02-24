00889a934cae2559182146a9640b901e6a4bffca
CygnerParthKanjariya <parth.kanjariya@cygner.in>
2026-02-24 17:13:07 +0530

    feat: Implement post fetching and display
    - Add `PostModel` to represent post data.
    - Create `ApiService` to fetch posts from a JSON placeholder API.
    - Update `HomePage` to use `FutureBuilder` to display a list of posts.
    - Add `DetailPage` to show the full title and body of a selected post.

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:api_demo/models/post_model.dart';
+import 'package:api_demo/services/api_service.dart';
 import 'package:flutter/material.dart';
+
+import 'detail_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,6 +21,44 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("API Demo"), centerTitle: true),
+      body: FutureBuilder<List<PostModel>>(
+        future: ApiService().fetchPosts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            print(snapshot.error);
+            print("no data");
+            return Center(child: Text("No Data"));
+          } else {
+            final posts = snapshot.data!;
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailPage(
+                          id: posts[index].id,
+                          title: posts[index].title,
+                          body: posts[index].body,
+                        ),
+                      ),
+                    );
+                  },
+                  leading: CircleAvatar(
+                    child: Text(posts[index].id.toString()),
+                  ),
+                  title: Text(posts[index].title, maxLines: 1),
+                  subtitle: Text(posts[index].body, maxLines: 1),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
